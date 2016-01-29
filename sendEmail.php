5ef7cf7915174440eb158005ed64ba10230893da
jfigu010 <jfigu010@ucr.edu>
2016-01-28 22:16:18 -0800

    try index d

--- sendEmail.php
+++ sendEmail.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_POST['submit']))
+if(!isset($_POST['send message']))
 {
 	//This page should be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
