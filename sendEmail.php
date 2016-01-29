d187f8c97bcd37f4984ab03f4b6048afec8b6a11
jfigu010 <jfigu010@ucr.edu>
2016-01-28 22:23:44 -0800

    try index dd

--- sendEmail.php
+++ sendEmail.php
@@ -1,30 +1,16 @@
 <?php
-if(!isset($_POST['send message']))
-{
-	//This page should be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+if(isset($_POST["submit"])) {
+$recipient = "josefigu010@gmail.com"; //my email
+echo $subject = 'Email message from Point Plumbing';
+echo $name = $_POST ["yourName"];
+echo $email = $_POST["yourEmail"];
+echo  $message = $_POST["yourMessage"];
+
+ $mailBody="Name: $name\nEmail: $email\n\n$message"; 
+
+ mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
+
 }
-$name = $_POST['name'];
-$visitor_email = $$_POST['email'];
-$message = $_POST['message'];
-
-//Validate first
-if(empty($name)||empty($visitor_email))
-{
-	echo "Name and email are mandatory";
-	exit;
-}
-
-$email_from = 'josefigu010@gmail.com';//My email address here
-$email_subject = "New form submission";
-$email_body = "You have received a new message from the user $name.\n".
-	"email address: $visitor_email\n".
-	"Here is the message:\n $message".
-	
-$to = "josefigu010@gmail.com";//My email address here
-$headers = "From: $email_from \r\n";
-
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to tahnk you page.
-</?php>
+?>
